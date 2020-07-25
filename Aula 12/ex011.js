530fa029c9a16fa316641d7013b348a5655ef864
Milena Carecho <37448340+MilenaCarecho@users.noreply.github.com>
2020-07-25 00:43:00 -0300

    Delete ex011.js

--- Aula 12/ex011.js
+++
@@ -1,8 +0,0 @@
-var idade = 45
-if (idade < 16){
-    console.log('Não vota')
-} else if (idade < 18 || idade > 65) {
-    console.log('Voto opcional')
-} else {
-    console.log('Voto obrigatorio')
-} 
