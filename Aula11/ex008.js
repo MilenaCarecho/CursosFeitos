9e9f2922aeb394e0f813d014ce9bf41b3875d60c
Milena Carecho <37448340+MilenaCarecho@users.noreply.github.com>
2020-07-25 00:43:12 -0300

    Delete ex008.js

--- Aula11/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 78
-console.log(`A velocidade do seu carro é ${vel}Km/h`)
-if (vel > 60){
-    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
-}
-console.log(`Dirija sempre usando cinto de segurança!`)
