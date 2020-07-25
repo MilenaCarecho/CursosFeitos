ca81a150e97fa6f53345ad03f18f83f06bb5b408
Milena Carecho <37448340+MilenaCarecho@users.noreply.github.com>
2020-07-25 00:43:33 -0300

    Create ex011.js

---
+++ CursoEmVideo/Aula 12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 45
+if (idade < 16){
+    console.log('Não vota')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto opcional')
+} else {
+    console.log('Voto obrigatorio')
+} 
